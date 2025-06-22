392db553762670aee21fb2d086a02af1c8c96376
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-22 01:28:36 -0700

    Initial commit

---
+++ src/main/java/net/thebrewingminer/dynamicoreveins/DynamicOreVeins.java
@@ -0,0 +1,43 @@
+package net.thebrewingminer.dynamicoreveins;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(DynamicOreVeins.MOD_ID)
+public class DynamicOreVeins{
+    public static final String MOD_ID = "dynamic_ore_veins";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DynamicOreVeins(FMLJavaModLoadingContext context){
+        IEventBus modEventBus = context.getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+        LOGGER.info("HELLO from common setup!!");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event){
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
