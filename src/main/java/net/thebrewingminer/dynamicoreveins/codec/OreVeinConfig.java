09c4dc8e6afa3ec9ea34b749675956f872813c78
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-22 04:09:52 -0700

    Setup config for block fields and dimension field. Density-function-related fields to do next.

---
+++ src/main/java/net/thebrewingminer/dynamicoreveins/codec/OreVeinConfig.java
@@ -0,0 +1,31 @@
+package net.thebrewingminer.dynamicoreveins.codec;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+
+public class OreVeinConfig {
+    public final BlockStateProvider ore;
+    public final BlockStateProvider raw_ore;
+    public final BlockStateProvider fillerBlock;
+    public final ResourceKey<Level> dimension;
+
+    public static final Codec<ResourceKey<Level>> DIMENSION_CODEC = ResourceKey.codec(Registry.DIMENSION_REGISTRY);
+    
+    public static final Codec<OreVeinConfig> CODEC = RecordCodecBuilder.create(oreVeinConfigInstance -> oreVeinConfigInstance.group(
+            BlockStateOrWeightedList.CODEC.fieldOf("ore").forGetter(config -> config.ore),
+            BlockStateOrWeightedList.CODEC.fieldOf("raw_ore").forGetter(config -> config.raw_ore),
+            BlockStateOrWeightedList.CODEC.fieldOf("filler_block").forGetter(config -> config.fillerBlock),
+            DIMENSION_CODEC.fieldOf("dimension").forGetter(config -> config.dimension)
+    ).apply(oreVeinConfigInstance, OreVeinConfig::new));
+
+    public OreVeinConfig(BlockStateProvider ore, BlockStateProvider rawOre, BlockStateProvider fillerBlock, ResourceKey<Level> dimension) {
+        this.ore = ore;
+        this.raw_ore = rawOre;
+        this.fillerBlock = fillerBlock;
+        this.dimension = dimension;
+    }
+}
