56bc94eb03a87e19e75bfea7b7c202ee991ca5bc
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-24 16:59:48 -0700

    Allowed direct inline/reference of density functions. Codec will wrap them into the full codec with default thresholds meant for testing.

--- src/main/java/net/thebrewingminer/dynamicoreveins/codec/DensityFunctionThreshold.java
+++ src/main/java/net/thebrewingminer/dynamicoreveins/codec/DensityFunctionThreshold.java
@@ -1,13 +1,28 @@
 package net.thebrewingminer.dynamicoreveins.codec;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.levelgen.DensityFunction;
 
 public record DensityFunctionThreshold(DensityFunction function, double minThreshold, double maxThreshold){
-    public static final Codec<DensityFunctionThreshold> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+    public static final double DEFAULT_MIN_THRESHOLD = -1.0;
+    public static final double DEFAULT_MAX_THRESHOLD = 1.0;
+
+    public static final Codec<DensityFunctionThreshold> DENSITY_FUNCTION_THRESHOLD_CODEC = RecordCodecBuilder.create(instance -> instance.group(
             DensityFunction.HOLDER_HELPER_CODEC.fieldOf("input").forGetter(DensityFunctionThreshold::function),
             Codec.DOUBLE.fieldOf("min_threshold").forGetter(DensityFunctionThreshold::minThreshold),
             Codec.DOUBLE.fieldOf("max_threshold").forGetter(DensityFunctionThreshold::maxThreshold)
     ).apply(instance, DensityFunctionThreshold::new));
+
+    public static final Codec<DensityFunctionThreshold> CODEC = Codec.either(
+            DensityFunction.HOLDER_HELPER_CODEC,
+            DENSITY_FUNCTION_THRESHOLD_CODEC
+    ).xmap(
+            either -> either.map(
+                    densityFunction -> new DensityFunctionThreshold(densityFunction, DEFAULT_MIN_THRESHOLD, DEFAULT_MAX_THRESHOLD),
+                    densityFunctionThreshold -> densityFunctionThreshold
+            ),
+            Either::right
+    );
 }
