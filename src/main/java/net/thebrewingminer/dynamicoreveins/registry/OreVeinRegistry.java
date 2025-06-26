6ec52744e509fa4d8dc8f6a92e4a1636e03b135d
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-25 22:35:05 -0700

    Commented out. Will be moving up to 1.19.3 for datapack Forge support.

--- src/main/java/net/thebrewingminer/dynamicoreveins/registry/OreVeinRegistry.java
+++ src/main/java/net/thebrewingminer/dynamicoreveins/registry/OreVeinRegistry.java
@@ -1,3 +1,21 @@
 package net.thebrewingminer.dynamicoreveins.registry;
 
-public class OreVeinRegistry{}
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DeferredRegister;
+import net.thebrewingminer.dynamicoreveins.DynamicOreVeins;
+import net.thebrewingminer.dynamicoreveins.codec.OreVeinConfig;
+
+//@Mod.EventBusSubscriber(modid = DynamicOreVeins.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class OreVeinRegistry{
+//    public static final DeferredRegister<OreVeinConfig> ORE_VEINS = DeferredRegister.create(new ResourceLocation(DynamicOreVeins.MOD_ID, "ore_vein"), DynamicOreVeins.MOD_ID);
+//
+//    @SubscribeEvent
+//    public static void registerDataPackRegistries(DataPackRegistryEvent.NewRegistry event) {
+//        event.dataPackRegistry(
+//                OreVeinRegistry.ORE_VEINS,
+//                OreVeinConfig.CODEC
+//        );
+//    }
+}
