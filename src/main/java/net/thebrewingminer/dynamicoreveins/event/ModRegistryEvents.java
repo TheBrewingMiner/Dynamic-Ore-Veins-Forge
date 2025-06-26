874fe2e4478187fcae75084fb6f27b18bf98c43c
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-26 00:39:40 -0700

    Refactored ORE_VEIN_REGISTRY_KEY to ORE_VEIN_REGISTRY

--- src/main/java/net/thebrewingminer/dynamicoreveins/event/ModRegistryEvents.java
+++ src/main/java/net/thebrewingminer/dynamicoreveins/event/ModRegistryEvents.java
@@ -11,6 +11,6 @@ import net.thebrewingminer.dynamicoreveins.registry.OreVeinRegistry;
 public class ModRegistryEvents {
     @SubscribeEvent
     public static void onRegisterDataPackRegistry(DataPackRegistryEvent.NewRegistry event) {
-        event.dataPackRegistry(OreVeinRegistry.ORE_VEIN_REGISTRY_KEY, OreVeinConfig.CODEC);
+        event.dataPackRegistry(OreVeinRegistry.ORE_VEIN_REGISTRY, OreVeinConfig.CODEC);
     }
 }
