c2c5cd047235cb39a24d4b69957776c0c16302ab
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-25 23:36:10 -0700

    Register registry for datapack use.

---
+++ src/main/java/net/thebrewingminer/dynamicoreveins/event/ModRegistryEvents.java
@@ -0,0 +1,16 @@
+package net.thebrewingminer.dynamicoreveins.event;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.DataPackRegistryEvent;
+import net.thebrewingminer.dynamicoreveins.DynamicOreVeins;
+import net.thebrewingminer.dynamicoreveins.codec.OreVeinConfig;
+import net.thebrewingminer.dynamicoreveins.registry.OreVeinRegistry;
+
+@Mod.EventBusSubscriber(modid = DynamicOreVeins.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModRegistryEvents {
+    @SubscribeEvent
+    public static void onRegisterDataPackRegistry(DataPackRegistryEvent.NewRegistry event) {
+        event.dataPackRegistry(OreVeinRegistry.ModRegistries.ORE_VEIN_REGISTRY_KEY, OreVeinConfig.CODEC);
+    }
+}
