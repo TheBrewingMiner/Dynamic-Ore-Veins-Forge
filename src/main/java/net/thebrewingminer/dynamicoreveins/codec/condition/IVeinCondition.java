724f10a3142a3f41148f4f3e2f4b62b0d667c11b
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-26 21:12:42 -0700

    Renamed parameter

--- src/main/java/net/thebrewingminer/dynamicoreveins/codec/condition/IVeinCondition.java
+++ src/main/java/net/thebrewingminer/dynamicoreveins/codec/condition/IVeinCondition.java
@@ -1,19 +1,17 @@
 package net.thebrewingminer.dynamicoreveins.codec.condition;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.LevelHeightAccessor;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.DensityFunction;
 
 public interface IVeinCondition {
     Codec<? extends IVeinCondition> codec();
     String type();
-    boolean test(Context context);
-    interface Context {
-        BlockPos pos();
-        LevelHeightAccessor heightAccessor();
-        ChunkGenerator chunkGenerator();
-        double compute(DensityFunction function);
-    }
+    boolean test(DensityFunction densityFunction);
+    /* Deprecated interface wrapper*/
+//    interface Context {
+//        BlockPos pos();
+//        LevelHeightAccessor heightAccessor();
+//        ChunkGenerator chunkGenerator();
+//        double compute(DensityFunction function);
+//    }
 }
