a0eea73c2e1bcee1bb099437a6b21a8544920ce1
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-06-26 21:19:15 -0700

    Implemented getter for registry.

---
+++ src/main/java/net/thebrewingminer/dynamicoreveins/registry/OreVeinRegistryHolder.java
@@ -0,0 +1,23 @@
+package net.thebrewingminer.dynamicoreveins.registry;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
+import net.thebrewingminer.dynamicoreveins.codec.OreVeinConfig;
+
+public class OreVeinRegistryHolder {
+    private static RegistryAccess access;
+    public static final ResourceKey<Registry<OreVeinConfig>> REGISTRY = OreVeinRegistry.ORE_VEIN_REGISTRY;
+
+    public static void init(RegistryAccess registryAccess) {
+        access = registryAccess;
+        Registry<OreVeinConfig> oreVeinRegistry = getRegistry();
+    }
+
+    public static Registry<OreVeinConfig> getRegistry() {
+        if (access == null) {
+            throw new IllegalStateException("RegistryAccess has not been initialized");
+        }
+        return access.registryOrThrow(REGISTRY);
+    }
+}
