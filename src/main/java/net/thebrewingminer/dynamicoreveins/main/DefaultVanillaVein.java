1a15be2109d2cdf05e3646788a8fb820872d1113
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-07-07 02:00:54 -0700

    Built default veins from Vanille enums as custom config objects. Will switch codec for vein- ridge and gap to the one used by vanilla as well.

---
+++ src/main/java/net/thebrewingminer/dynamicoreveins/main/DefaultVanillaVein.java
@@ -0,0 +1,50 @@
+package net.thebrewingminer.dynamicoreveins.main;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.thebrewingminer.dynamicoreveins.codec.OreRichnessSettings;
+import net.thebrewingminer.dynamicoreveins.codec.OreVeinConfig;
+import net.thebrewingminer.dynamicoreveins.codec.condition.DensityFunctionThreshold;
+import net.thebrewingminer.dynamicoreveins.codec.condition.HeightRangeCondition;
+
+import java.util.Collections;
+
+public class DefaultVanillaVein {
+    private DefaultVanillaVein(){}
+
+    protected OreVeinConfig IRON_VEIN = new OreVeinConfig(
+        BlockStateProvider.simple(Blocks.DEEPSLATE_IRON_ORE),
+        BlockStateProvider.simple(Blocks.RAW_IRON_BLOCK),
+    0.02f,
+        BlockStateProvider.simple(Blocks.TUFF),
+        new DensityFunctionThreshold(null, Double.NEGATIVE_INFINITY, 0.0),
+        new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD),
+        new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD),
+        OreRichnessSettings.createDefault(),
+        Collections.singletonList(Level.OVERWORLD),
+        new HeightRangeCondition(VerticalAnchor.absolute(-60), VerticalAnchor.absolute(-8))
+    );
+
+    protected OreVeinConfig COPPER_VEIN = new OreVeinConfig(
+            BlockStateProvider.simple(Blocks.COPPER_ORE),
+            BlockStateProvider.simple(Blocks.RAW_COPPER_BLOCK),
+            0.02f,
+            BlockStateProvider.simple(Blocks.GRANITE),
+            new DensityFunctionThreshold(null, 0.0, Double.POSITIVE_INFINITY),
+            new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD),
+            new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD),
+            OreRichnessSettings.createDefault(),
+            Collections.singletonList(Level.OVERWORLD),
+            new HeightRangeCondition(VerticalAnchor.absolute(0), VerticalAnchor.absolute(50))
+    );
+
+    public OreVeinConfig ironVein(){
+        return IRON_VEIN;
+    }
+
+    public OreVeinConfig copperVein(){
+        return COPPER_VEIN;
+    }
+}
