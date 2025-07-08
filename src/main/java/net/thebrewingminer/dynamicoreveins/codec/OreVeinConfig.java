416e00c3fb20580f21f6094dde377f4f5f3c79ca
TheBrewingMiner <130880104+TheBrewingMiner@users.noreply.github.com>
2025-07-07 17:22:17 -0700

    Converted config to a record.

--- src/main/java/net/thebrewingminer/dynamicoreveins/codec/OreVeinConfig.java
+++ src/main/java/net/thebrewingminer/dynamicoreveins/codec/OreVeinConfig.java
@@ -14,41 +14,17 @@ import net.thebrewingminer.dynamicoreveins.registry.VeinConditionRegistry;
 import java.util.Collections;
 import java.util.List;
 
-public class OreVeinConfig {
-    public final BlockStateProvider ore;
-    public final BlockStateProvider secondary_ore;
-    public final float secondary_ore_chance;
-    public final BlockStateProvider fillerBlock;
-    public final OreRichnessSettings veinSettings;
-    public final List<ResourceKey<Level>> dimension;
-    public final DensityFunctionThreshold veinToggle;
-    public final DensityFunctionThreshold veinRidged;
-    public final DensityFunctionThreshold veinGap;
-    public final IVeinCondition conditions;
-
+public record OreVeinConfig(BlockStateProvider ore, BlockStateProvider secondaryOre, float secondaryOreChance, BlockStateProvider fillerBlock, DensityFunctionThreshold veinToggle, DensityFunctionThreshold veinRidged, DensityFunctionThreshold veinGap, OreRichnessSettings veinSettings, List<ResourceKey<Level>> dimension, IVeinCondition conditions){
     public static final Codec<OreVeinConfig> CODEC = RecordCodecBuilder.create(oreVeinConfigInstance -> oreVeinConfigInstance.group(
-        ResourceKeyOrBlockState.CODEC.fieldOf("ore").forGetter(config -> config.ore),
-        ResourceKeyOrBlockState.CODEC.fieldOf("secondary_ore").forGetter(config -> config.secondary_ore),
-        Codec.floatRange(0.0f, 1.0f).fieldOf("secondary_ore_chance").orElse(0.02f).forGetter(config -> config.secondary_ore_chance),
+        ResourceKeyOrBlockState.CODEC.fieldOf("ore").forGetter(OreVeinConfig::ore),
+        ResourceKeyOrBlockState.CODEC.fieldOf("secondary_ore").forGetter(OreVeinConfig::secondaryOre),
+        Codec.floatRange(0.0f, 1.0f).fieldOf("secondary_ore_chance").orElse(0.02f).forGetter(OreVeinConfig::secondaryOreChance),
         ResourceKeyOrBlockState.CODEC.fieldOf("filler_block").forGetter(config -> config.fillerBlock),
-        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_toggle", new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD)).forGetter(config -> config.veinToggle),
-        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_ridged", new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD)).forGetter(config -> config.veinRidged),
-        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_gap", new DensityFunctionThreshold(null, DensityFunctionThreshold.DEFAULT_MIN_THRESHOLD, DensityFunctionThreshold.DEFAULT_MAX_THRESHOLD)).forGetter(config -> config.veinGap),
-        OreRichnessSettings.CODEC.optionalFieldOf("vein_settings", OreRichnessSettings.createDefault()).forGetter(config -> config.veinSettings),
-        IsDimension.CODEC.fieldOf("dimension").orElse(Collections.singletonList(Level.OVERWORLD)).forGetter(config -> config.dimension),
-        VeinConditionRegistry.codec().optionalFieldOf("conditions", new AlwaysTrueCondition()).forGetter(config -> config.conditions)
+        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_toggle", DensityFunctionThreshold.createDefault()).forGetter(OreVeinConfig::veinToggle),
+        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_ridged", DensityFunctionThreshold.createDefault()).forGetter(OreVeinConfig::veinRidged),
+        DensityFunctionThreshold.CODEC.optionalFieldOf("vein_gap", DensityFunctionThreshold.createDefault()).forGetter(OreVeinConfig::veinGap),
+        OreRichnessSettings.CODEC.optionalFieldOf("vein_settings", OreRichnessSettings.createDefault()).forGetter(OreVeinConfig::veinSettings),
+        IsDimension.CODEC.fieldOf("dimension").orElse(Collections.singletonList(Level.OVERWORLD)).forGetter(OreVeinConfig::dimension),
+        VeinConditionRegistry.codec().optionalFieldOf("conditions", new AlwaysTrueCondition()).forGetter(OreVeinConfig::conditions)
     ).apply(oreVeinConfigInstance, OreVeinConfig::new));
-
-    public OreVeinConfig(BlockStateProvider ore, BlockStateProvider secondaryOre, float secondaryOreChance, BlockStateProvider fillerBlock, DensityFunctionThreshold veinToggle, DensityFunctionThreshold veinRidged, DensityFunctionThreshold veinGap, OreRichnessSettings veinSettings, List<ResourceKey<Level>> dimension, IVeinCondition conditions){
-        this.ore = ore;
-        this.secondary_ore = secondaryOre;
-        this.secondary_ore_chance = secondaryOreChance;
-        this.fillerBlock = fillerBlock;
-        this.veinToggle = veinToggle;
-        this.veinRidged = veinRidged;
-        this.veinGap = veinGap;
-        this.veinSettings = veinSettings;
-        this.dimension = dimension;
-        this.conditions = conditions;
-    }
 }
